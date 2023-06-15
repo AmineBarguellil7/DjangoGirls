0fc438806159c37907d8c5377df37fe32c27c8b3
Amine <amine.barguellil@esprit.tn>
2023-06-15 16:48:08 +0100

    Sec

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
    path('index/', ListPost, name='ListPost'),
    path('post/<int:pk>/', post_detail, name='post_detail'),
    path('post/new/', post_new, name='post_new'),
+   path('post/<int:pk>/edit/', post_edit, name='post_edit'),
 ]
