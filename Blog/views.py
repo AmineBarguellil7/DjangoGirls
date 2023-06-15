80ea127e4b57e724776f2fb1a52fe4a624074d21
Amine <amine.barguellil@esprit.tn>
2023-06-15 13:15:33 +0100

    Change

--- Blog/views.py
+++ Blog/views.py
@@ -5,5 +5,5 @@ def ListPost(request):
     Posts=Post.objects.all()
     return render(request, 'Blog/affiche.html',{'p':Posts})
 
-def index(request):
-    return render(request, 'Blog/index.html')
+# def index(request):
+#     return render(request, 'Blog/index.html')
--- Test/urls.py
+++ Test/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='blog/index', permanent=True)),
     path('admin/', admin.site.urls),
     path('blog/', include('Blog.urls')),
 ]
