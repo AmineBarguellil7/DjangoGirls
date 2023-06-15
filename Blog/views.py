8ea7b12d8bf9eb0676f293ff83eeef7d41ba2223
Amine <amine.barguellil@esprit.tn>
2023-06-15 12:17:21 +0100

    DjangoUp🚀

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *  
 
-# Create your views here.
+def ListPost(request):
+    Posts=Post.objects.all()
+    return render(request, 'Blog/affiche.html',{'p':Posts})
+
+def index(request):
+    return render(request, 'Blog/index.html')
