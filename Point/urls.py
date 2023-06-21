232f1c55ec60b0d21457c5664dc73e6a06ed7af8
Amine <amine.barguellil@esprit.tn>
2023-06-21 13:18:46 +0100

    'Login🚀'

--- Point/urls.py
+++ Point/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from .views import  *
 
 
-urlpatterns=[
-   path('index/',ShowMap, name='ShowMap'),
-   path('save_marker/', save_marker, name='save_marker')
+
+urlpatterns = [
+   path('index/', ShowMap, name='ShowMap'),
+   path('save_marker/', save_marker, name='save_marker'),
+   path('home/',Home,name='Home'),
+   path('signUp/',SignUp, name='SignUp'),
+   path('login/', Login, name='login'),
+   path('logout/', Logout, name='logout'),
 ]
--- Point/views.py
+++ Point/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Marker
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 from django.http import JsonResponse
 import json
 
 from django.contrib.gis.geos import Point
 from django.core import serializers
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import *
+from django.urls import reverse_lazy
+
+from django.contrib import messages
+
+
 
 
 def save_marker(request):
@@ -26,9 +36,71 @@ def save_marker(request):
 
 
 def ShowMap(request):
-    markers = Marker.objects.all()
-    markers_json = serializers.serialize('json', markers)
-    return render(request, 'Point/map.html', {'markers': markers_json})
+    if request.user.is_authenticated:
+        markers = Marker.objects.all()
+        markers_json = serializers.serialize('json', markers)
+        return render(request, 'Point/map.html', {'markers': markers_json})
+    else:
+        return render(request, 'Point/error.html')    
+
+
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect("ShowMap")
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return redirect('Home')    
+    return render(request,'Point/login.html')
+
+def SignUp(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username.")
+            return redirect('Home')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!!")
+            return redirect('Home')
+        
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't matched!!")
+            return redirect('Home')
+        
+        
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        #myuser.is_active = False
+        myuser.save()
+        messages.success(request, "Your Account has been created succesfully!!")
+        return redirect('login')
+    return render(request, "Point/SignUp.html")    
+         
+
+def Home(request):
+    return render(request,"Point/Home.html")  
+
+
+def Logout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!!")
+    return redirect('Home') 
+
 
    
 
