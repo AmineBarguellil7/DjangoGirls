9587cebb016c8549f6e3f3ece50ea8c3e5f36bb3
Amine <amine.barguellil@esprit.tn>
2023-07-08 17:35:15 +0100

    UpRepo🚀

--- Point/views.py
+++ Point/views.py
@@ -85,7 +85,6 @@ def SignUp(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        #myuser.is_active = False
         myuser.save()
         messages.success(request, "Your Account has been created succesfully!!")
         return redirect('login')
